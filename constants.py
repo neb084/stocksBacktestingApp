eb2c7562bb554d727b3c8d8e488de2cac8a4473f
neb084 <bspellman84@gmail.com>
2020-09-27 19:14:57 -0700

    testing historical & analytical data lists & dictionaries

--- constants.py
+++ constants.py
@@ -1,9 +1,25 @@
+# Millisecond conversions
 MILLISECONDS_PER_SECOND = 1000
 MILLISECONDS_PER_MINUTE = 60000
 MILLISECONDS_PER_HOUR = 3600000
 MILLISECONDS_PER_DAY = 86400000
 MILLISECONDS_PER_YEAR_365 = MILLISECONDS_PER_DAY * 365  # Assumes 365 days in year
+
+# Over & Under Analysis comparisons
 OVER = "OVER"
 UNDER = "UNDER"
 EQUALS = "EQUALS"
 NA = "N/A"
+
+# Analysis_Type codes
+ANALYSIS_2MA = "2 Moving Averages Analysis"
+
+# Buy & Sell signal coding
+BUY_OPEN_CURRENT_DAY = "BUY on the OPEN of the current day"
+BUY_CLOSE_CURRENT_DAY = "BUY on the CLOSE of the current day"
+BUY_OPEN_NEXT_DAY = "BUY on the OPEN of the next day"
+BUY_CLOSE_NEXT_DAY = "BUY on the CLOSE of the next day"
+SELL_OPEN_CURRENT_DAY = "SELL on the OPEN of the current day"
+SELL_CLOSE_CURRENT_DAY = "SELL on the CLOSE of the current day"
+SELL_OPEN_NEXT_DAY = "SELL on the OPEN of the next day"
+SELL_CLOSE_NEXT_DAY = "SELL on the CLOSE of the next day"
